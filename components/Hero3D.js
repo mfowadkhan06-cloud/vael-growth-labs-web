0fadac90e87c843c83f89243366ebed31945449e
mfowadkhan06-cloud <mfowadkhan06@gmail.com>
2026-03-18 22:07:38 +0530

    Update Hero3D.js

--- components/Hero3D.js
+++ components/Hero3D.js
@@ -1,9 +1,26 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Float } from "@react-three/drei";
+
+function Box() {
+  return (
+    <Float speed={2} rotationIntensity={1.5}>
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color="#C79F1F" />
+      </mesh>
+    </Float>
+  );
+}
+
 export default function Hero3D() {
   return (
-    <div style={{height: "400px", background: "#002855", color: "white"}}>
-      <h2 style={{padding: "100px", textAlign: "center"}}>
-        3D City View (Advanced version coming)
-      </h2>
-    </div>
+    <Canvas style={{ height: "400px" }}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[2, 2, 5]} />
+      <Box />
+      <OrbitControls enableZoom={false} />
+    </Canvas>
   );
 }
